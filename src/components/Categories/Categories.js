50815280fb86c381f20bf0998e86853690676dea
tikmanasyan <manasyan.tigran@mail.ru>
2023-01-12 17:58:34 +0400

    Class Component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -3,16 +3,39 @@ import React from "react";
 class Categories extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            counter:0,
+            users:[],
+            posts:[]
+        }
+        this.handleInc = this.handleInc.bind(this);
     }
 
     componentWillUnmount() {
         console.log("Component Categories deleted");
     } 
 
+    handleInc() {
+        console.log(this)
+        this.setState({
+            ...this.state,
+            counter: this.state.counter + 1
+        });
+    }
+
+    handleDec = () => {
+        this.setState({
+            ...this.state,
+            counter: this.state.counter - 1
+        });
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello, I am class component</h1>
+                <h1>{this.state.counter}</h1>
+                <button onClick={this.handleInc}>+</button>
+                <button onClick={this.handleDec}>-</button>
             </div>
         );
     }
