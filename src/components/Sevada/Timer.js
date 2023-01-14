afab05439cf46e9455591443030d1ad9024f9d7e
Sevada20 <poxosyansevada@gmail.com>
2023-01-15 03:55:14 +0400

    TimerButtons

--- src/components/Sevada/Timer.js
+++ src/components/Sevada/Timer.js
@@ -6,31 +6,49 @@ class Timer extends React.Component {
       second: 0,
       minute: 0,
       hour: 0,
-      start: false,
     };
-  }  
+  }
 
-  componentDidMount() {
-  setInterval(() => {
+  pauseTime = () => {
+    clearInterval(this.interval);
+  };
+
+  nullTime = () => {
+    this.setState({
+      ...this.state,
+      second: 0,
+      minute: 0,
+      hour: 0,
+    });
+  };
+
+  start = () => {
+    this.interval = setInterval(() => {
       if (this.state.second < 5) {
-        this.setState({ second: this.state.second + 1 });
+        this.setState({ ...this.state, second: this.state.second + 1 });
       } else if (this.state.second === 5) {
-        this.setState({ second: 0 });
-        this.setState({ minute: this.state.minute + 1 });
+        this.setState({
+          ...this.state,
+          second: 0,
+          minute: this.state.minute + 1,
+        });
         if (this.state.minute === 2) {
-          this.setState({ minute: 0 });
-          this.setState({ hour: this.state.hour + 1 });
+          this.setState({
+            ...this.state,
+            minute: 0,
+            hour: this.state.hour + 1,
+          });
         }
       }
     }, 1000);
-  }
+  };
 
   render() {
     return (
       <div>
-        <button>Start</button>
-        <button>Pause</button>
-        <button>null</button>
+        <button onClick={this.start}>Start</button>
+        <button onClick={this.pauseTime}>Pause</button>
+        <button onClick={this.nullTime}>null</button>
         <br />
         {this.state.hour} : {this.state.minute} : {this.state.second}
       </div>
